3840eac918298f749c48518fa76cd1825cf949d7
tdoddareddy <tdoddareddy@pivotal.io>
2019-08-15 17:14:30 -0500

    fixed skipper

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -10,4 +10,4 @@ wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipp
 cp manifests/scdf-server-manifest.yml  build-output
 cp manifests/scdf-skipper-manifest.yml build-output
 cp spring-cloud-dataflow-server-2.1.2.RELEASE.jar build-output
-cp spring-cloud-dataflow-skipper-2.0.3.RELEASE.jar build-output
+cp spring-cloud-skipper-server-2.0.3.RELEASE.jar build-output
