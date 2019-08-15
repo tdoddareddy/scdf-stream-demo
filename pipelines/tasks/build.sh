ed736779a19dcb0854f02472851818c370d03d2c
tdoddareddy <tdoddareddy@pivotal.io>
2019-08-15 17:18:30 -0500

    fixed issues

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -2,12 +2,12 @@
 
 set -e -x
 
-cd scdf-stream-demo
 mkdir build-output
+cd scdf-stream-demo
 pwd
 wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.1.2.RELEASE/spring-cloud-dataflow-server-2.1.2.RELEASE.jar
 wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipper/2.0.3.RELEASE/spring-cloud-skipper-2.0.3.RELEASE.jar
-cp manifests/scdf-server-manifest.yml  build-output
-cp manifests/scdf-skipper-manifest.yml build-output
-cp spring-cloud-dataflow-server-2.1.2.RELEASE.jar build-output
-cp spring-cloud-skipper-server-2.0.3.RELEASE.jar build-output
+cp manifests/scdf-server-manifest.yml ../build-output
+cp manifests/scdf-skipper-manifest.yml ../build-output
+cp spring-cloud-dataflow-server-2.1.2.RELEASE.jar ../build-output
+cp spring-cloud-skipper-2.0.3.RELEASE.jar ../build-output
