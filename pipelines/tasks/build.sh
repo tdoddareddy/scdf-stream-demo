2fef4edc582bf2b70bbf6e00f94f0c3918706fca
tdoddareddy <tdoddareddy@pivotal.io>
2019-08-15 16:38:24 -0500

    first commit

---
+++ pipelines/tasks/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e -x
+
+cd scdf-stream-demo
+
+wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.1.2.RELEASE/spring-cloud-dataflow-server-2.1.2.RELEASE.jar build-output/.
+wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipper/2.0.3.RELEASE/spring-cloud-skipper-2.0.3.RELEASE.jar build-output/.
+cp pipelines/manifests/scdf-server-manifest.yml  build-output/.
+cp pipelines/manifests/scdf-skipper-manifest.yml build-output/.
