956839686e10ee0bca0839f6fb27a6b316c13f6b
tdoddareddy <tdoddareddy@pivotal.io>
2019-08-15 17:06:59 -0500

    fixed commands

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -5,9 +5,9 @@ set -e -x
 cd scdf-stream-demo
 mkdir build-output
 pwd
-wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.1.2.RELEASE/spring-cloud-dataflow-server-2.1.2.RELEASE.jar .
-wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipper/2.0.3.RELEASE/spring-cloud-skipper-2.0.3.RELEASE.jar .
-cp pipelines/manifests/scdf-server-manifest.yml  build-output/.
-cp pipelines/manifests/scdf-skipper-manifest.yml build-output/.
+wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.1.2.RELEASE/spring-cloud-dataflow-server-2.1.2.RELEASE.jar
+wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipper/2.0.3.RELEASE/spring-cloud-skipper-2.0.3.RELEASE.jar
+cp pipelines/manifests/scdf-server-manifest.yml  build-output
+cp pipelines/manifests/scdf-skipper-manifest.yml build-output
 cp spring-cloud-dataflow-server-2.1.2.RELEASE.jar build-output
 cp spring-cloud-dataflow-skipper-2.0.3.RELEASE.jar build-output
