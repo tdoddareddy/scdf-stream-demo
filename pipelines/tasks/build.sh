30003b3525f1bb6890f3a4e53cf6b6853622259a
tdoddareddy <tdoddareddy@pivotal.io>
2019-08-15 16:58:47 -0500

    added mkdir

--- pipelines/tasks/build.sh
+++ pipelines/tasks/build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 cd scdf-stream-demo
-
+mkdir build-output
 wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-dataflow-server/2.1.2.RELEASE/spring-cloud-dataflow-server-2.1.2.RELEASE.jar build-output/.
 wget https://repo1.maven.org/maven2/org/springframework/cloud/spring-cloud-skipper/2.0.3.RELEASE/spring-cloud-skipper-2.0.3.RELEASE.jar build-output/.
 cp pipelines/manifests/scdf-server-manifest.yml  build-output/.
